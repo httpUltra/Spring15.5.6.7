4d5aa09668b2eb581fbabb3e8d41cdf821269238
Ultra Lord <haxutoasty@gmail.com>
2023-08-19 12:16:58 -0500

    Added Object Enhancement exercise

---
+++ scripts.js
@@ -0,0 +1,29 @@
+// const createInstructor = (firstName, lastName) => {
+//     return {firstName, lastName}
+// };
+
+// const instructor = {
+//     firstName: "Christian",
+//     sayHi(){
+//         return "hi"
+//     },
+//     sayBye(){
+//         return this.firstName + " says bye"
+//     }
+// }
+
+// const favoriteNumber = 42;
+// const instructor ={
+//     firstName: "Colt",
+//     [favoriteNumber]: "That is my favorite"
+// };
+
+// function createAnimal (specie, verb, noise){
+//     return {
+//         specie,
+//         [verb](){
+//             return noise;
+//         }
+//     }
+// }
+
